8c71943dd745e17e396d7cd006403dcc796e9d66
Kipsazz <kbickus@gmail.com>
2020-06-29 23:21:40 +0300

    PHP 01 assignment

--- PHP1/index01.php
+++ PHP1/index01.php
@@ -1,37 +1,36 @@
 <?php
-	$title = 'PHP 01';
+	$title = 'PHP 01 PHP 01 assignment';
 ?>
 <!DOCTYPE html>
 <html lang="lt">
-<head>
-				<meta charset="utf-8">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<title><?php echo $title ?></title>
-</head>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title><?php echo $title ?></title>
+	</head>
 
-<body>
-	<?php
-	$salis = 'Lietuva';
-	$miestas = 'Vilnius';
-	$populiacija = '574221'
-	?>
-	<ul>
-		<li>
-			<?php
-			 echo $salis;
-			?>
-		</li>
-		<li>
-			<?php
-			echo $miestas;
-			?>
-		</li>
-		<li>
-			<?php
-			echo $populiacija;
-			?>
-		</li>
-	</ul>
-
-</body>
+	<body>
+		<?php
+		$salis = 'Lietuva';
+		$miestas = 'Vilnius';
+		$populiacija = '574221'
+		?>
+		<ul>
+			<li>
+				<?php
+				 echo $salis;
+				?>
+			</li>
+			<li>
+				<?php
+				echo $miestas;
+				?>
+			</li>
+			<li>
+				<?php
+				echo $populiacija;
+				?>
+			</li>
+		</ul>
+	</body>
 </html>
